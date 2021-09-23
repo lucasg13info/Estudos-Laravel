d731a277c49a601c48ec511aba50beb5422e45ae
Lucas Estefano <lucas.estefano@oi.net.br>
2021-09-23 10:18:15 -0300

    Criando views e passando na rota

--- routes/web.php
+++ routes/web.php
@@ -23,23 +23,27 @@ Route::get('/ola/{nome}', function($nome){
     echo "Olá! Seja bem vindon " . $nome . "!";
 });
 
+
+//Expressores regulares na rota para só funcionar em um range de letras e números
 Route::get('/rotacomregras/{nome}/{n}', function($nome, $n){
     for($i = 0; $i <$n; $i ++)
         echo "Olá! Seja bem vindo, $nome !<br>";
 })->where('nome', '[A-Za-z]+')->where ('n', '[0-9]+');
 
 
+
+//Agrupamento de rotas
 Route::prefix('/app')->group(function() {
     Route::get('/', function(){
-        return "Meu App";
+        return view('app');
     });
 
     Route::get('/user', function(){
-        return "User";
+        return view("user");
     });
 
     Route::get('/profile', function(){
-        return "Profile";
+        return view('profile');
     });
 });
 
